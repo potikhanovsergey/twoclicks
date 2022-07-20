4f7ad7360e5409a5fbb48bd135efd0e00eb9ecc9
sp <sp@outsi.de>
2022-07-20 21:03:59 +0300

    minor fixes

--- app/liked-blocks/mutations/deleteLikedBlock.ts
+++ app/liked-blocks/mutations/deleteLikedBlock.ts
@@ -4,10 +4,12 @@ import db from "db"
 export default async function DeleteLikedBlock(input, ctx: Ctx) {
   ctx.session.$isAuthorized()
 
-  const buildingBlock = await db.likedBlock.deleteMany({
+  const buildingBlock = await db.likedBlock.delete({
     where: {
-      buildingBlockId: input.buildingBlockId as string,
-      userId: ctx.session.userId as string,
+      userId_buildingBlockId: {
+        buildingBlockId: input.buildingBlockId as string,
+        userId: ctx.session.userId as string,
+      },
     },
   })
 
