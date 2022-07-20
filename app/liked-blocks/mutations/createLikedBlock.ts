37062bc2cb8fd604ada8a2c639abd640e9504c41
sp <sp@outsi.de>
2022-07-20 18:59:42 +0300

    change create to upsert when creating liked block

--- app/liked-blocks/mutations/createLikedBlock.ts
+++ app/liked-blocks/mutations/createLikedBlock.ts
@@ -5,11 +5,18 @@ export default async function CreateLikedBlock(input, ctx: Ctx) {
   ctx.session.$isAuthorized()
   const userId = ctx.session.userId as string
   const buildingBlockId = input.buildingBlockId as string
-  const likedBlock = await db.likedBlock.create({
-    data: {
+  const likedBlock = await db.likedBlock.upsert({
+    where: {
+      userId_buildingBlockId: {
+        userId,
+        buildingBlockId,
+      },
+    },
+    create: {
       userId,
       buildingBlockId,
     },
+    update: {},
   })
   return likedBlock
 }
