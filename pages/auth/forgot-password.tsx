008ff568dc7754eaf8af54c42b424021d39bd4cf
sp <sp@outsi.de>
2022-07-24 15:33:05 +0300

    todo: forget-password page/form

--- pages/auth/forgot-password.tsx
+++ pages/auth/forgot-password.tsx
@@ -1,6 +1,4 @@
 import Layout from "app/core/layouts/Layout"
-import { LabeledTextField } from "app/core/components/LabeledTextField"
-import { Form, FORM_ERROR } from "app/core/components/Form"
 import { ForgotPassword } from "app/auth/validations"
 import forgotPassword from "app/auth/mutations/forgotPassword"
 import { useMutation } from "@blitzjs/rpc"
@@ -21,22 +19,24 @@ const ForgotPasswordPage = () => {
           </p>
         </div>
       ) : (
-        <Form
-          submitText="Send Reset Password Instructions"
-          schema={ForgotPassword}
-          initialValues={{ email: "" }}
-          onSubmit={async (values) => {
-            try {
-              await forgotPasswordMutation(values)
-            } catch (error: any) {
-              return {
-                [FORM_ERROR]: "Sorry, we had an unexpected error. Please try again.",
-              }
-            }
-          }}
-        >
-          <LabeledTextField name="email" label="Email" placeholder="Email" />
-        </Form>
+        // todo: forget password form
+        <></>
+        // <Form
+        //   submitText="Send Reset Password Instructions"
+        //   schema={ForgotPassword}
+        //   initialValues={{ email: "" }}
+        //   onSubmit={async (values) => {
+        //     try {
+        //       await forgotPasswordMutation(values)
+        //     } catch (error: any) {
+        //       return {
+        //         [FORM_ERROR]: "Sorry, we had an unexpected error. Please try again.",
+        //       }
+        //     }
+        //   }}
+        // >
+        //   <LabeledTextField name="email" label="Email" placeholder="Email" />
+        // </Form>
       )}
     </Layout>
   )
