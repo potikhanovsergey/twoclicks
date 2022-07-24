f6489d333a6be373577fe61f42243d36994be06a
sp <sp@outsi.de>
2022-07-24 14:45:21 +0300

    remove noflash.js

--- public/noflash.js
+++
@@ -1,6 +0,0 @@
-;(function initTheme() {
-  const theme = localStorage.getItem("skillcase-color-scheme").replace(/"/g, "") || "light"
-  if (theme === "dark") {
-    document.querySelector("html").classList.add(theme)
-  }
-})()
