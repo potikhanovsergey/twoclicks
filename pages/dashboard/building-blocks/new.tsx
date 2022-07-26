013dd6e24d5051db595b5208c9388c3e940f1a23
sp <sp@outsi.de>
2022-07-26 22:40:28 +0300

    Remove import of unexisted component

--- pages/dashboard/building-blocks/new.tsx
+++ pages/dashboard/building-blocks/new.tsx
@@ -3,21 +3,10 @@ import { useRouter } from "next/router"
 import { useMutation } from "@blitzjs/rpc"
 import Layout from "app/core/layouts/Layout"
 import createBuildingBlock from "app/dashboard/building-blocks/mutations/createBuildingBlock"
-import { PortfolioMock } from "db/mocks"
-import createPortfolio from "app/dashboard/portfolios/mutations/createPortfolio"
 
 const NewBuildingBlockPage = () => {
   const router = useRouter()
   const [createBuildingBlockMutation] = useMutation(createBuildingBlock)
-  // const [createPortfolioMutation] = useMutation(createPortfolio)
-
-  // const handleCreatePortfolio = () => {
-  //   void createPortfolioMutation({
-  //     name: PortfolioMock.name,
-  //     data: deflate(PortfolioMock.data),
-  //   })
-  // }
-
   return (
     <Layout title={"Create New BuildingBlock"}>
       <h1>Create New BuildingBlock</h1>
