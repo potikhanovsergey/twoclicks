53e3439ff5f5bc46dc661ddc0e6053445e279d6e
sp <sp@outsi.de>
2022-08-03 03:36:50 +0300

    View component's json on click in dashboard

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,54 +1,75 @@
 import { getBaseLayout } from "app/core/layouts/BaseLayout"
-import { Container, Group, Space, Title, useMantineTheme } from "@mantine/core"
+import { Button, Container, Group, Space, Title, useMantineTheme } from "@mantine/core"
 import { GetServerSidePropsContext } from "next"
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"
-import React, { useCallback, useState } from "react"
+import React, { useCallback, useEffect, useState } from "react"
 import CodeMirror from "@uiw/react-codemirror"
 import { javascript } from "@codemirror/lang-javascript"
 import { serialize } from "helpers"
 import { jsonLanguage } from "@codemirror/lang-json"
+import FirstHero from "app/build/sections/FirstHero"
+
+const sections = [FirstHero]
 
 const DashboardIndex = () => {
-  const [value, setValue] = useState("")
-  const [json, setJson] = useState(
-    JSON.stringify(
-      JSON.parse(
-        serialize(
-          <Container size="xl">
-            <Title mb="xl">Dashboard</Title>
-            <Group position="apart" grow style={{ minHeight: "480px" }} align="stretch"></Group>
-          </Container>
-        )
-      ),
-      null,
-      2
-    )
-  )
+  const getJsonValue = (component) => {
+    return JSON.stringify(JSON.parse(serialize(component)), null, 2)
+  }
 
-  const onChange = useCallback((value, viewUpdate) => {
-    try {
-      const parsedJSON = JSON.stringify(JSON.parse(value), null, 2)
-      setJson(parsedJSON)
-    } catch (e) {}
-  }, [])
+  // const [json, setJson] = useState(
+  //   getJsonValue(
+  //     <Container size="xl">
+  //       <Title mb="xl">Dashboard</Title>
+  //       <Group position="apart" grow style={{ minHeight: "480px" }} align="stretch"></Group>
+  //     </Container>
+  //   )
+  // )
+  const [json, setJson] = useState("")
+
+  const [selectedSection, setSelectedSection] = useState<number | null>(null)
+
+  // const onChange = (value) => {
+  //   try {
+  //     const parsedJSON = JSON.stringify(JSON.parse(value), null, 2)
+  //     setJson(parsedJSON)
+  //   } catch (e) {}
+  // }
 
   const theme = useMantineTheme()
 
+  const handleButtonClick = (selected) => {
+    if (selected !== null) {
+      setSelectedSection(selected)
+      const invokedComponent = sections[selected]?.()
+      if (invokedComponent) {
+        setJson(getJsonValue(invokedComponent))
+      }
+    }
+  }
   return (
     <>
       <Space h="xl" />
       <Container size="xl">
         <Title mb="xl">Dashboard</Title>
-        <Group position="apart" grow style={{ minHeight: "480px" }} align="stretch">
+        <Group mb="xl">
+          {sections.map((S, i) => (
+            <Button key={i} color="red" onClick={() => handleButtonClick(i)}>
+              {S.name}
+            </Button>
+          ))}
+        </Group>
+        <Group position="apart" grow style={{ minHeight: "480px" }} align="stretch" mb="xl">
           <CodeMirror
             theme={theme.colorScheme}
             value={json}
             height="100%"
-            onChange={onChange}
+            // onChange={onChange}
+            editable={false}
             extensions={[jsonLanguage]}
             indentWithTab
           />
         </Group>
+        <>{selectedSection !== null && sections?.[selectedSection]?.()}</>
       </Container>
     </>
   )
