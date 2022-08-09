4bd59668a6986860ba529a4e502e9f587ecaf02a
DaryaSsss <d.semina10@gmail.com>
2022-08-09 19:42:17 +0300

    mantine-upgrade

--- app/build/BuilderHeader.tsx
+++ app/build/BuilderHeader.tsx
@@ -89,7 +89,7 @@ const BuilderHeader = ({ className }: { className?: string }) => {
               </Group>
             </Group>
           </Group>
-          <Group spacing={4}>
+          <Group spacing={8}>
             <ActionIcon onClick={toggle} color="violet" variant="filled">
               {fullscreen ? <AiOutlineFullscreenExit /> : <AiOutlineFullscreen />}
             </ActionIcon>
