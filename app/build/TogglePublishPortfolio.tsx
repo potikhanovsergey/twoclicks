e89db0914cee61013fbc662a115a29697acf6a65
sp <sp@outsi.de>
2022-08-11 14:33:13 +0300

    Fix switch

--- app/build/TogglePublishPortfolio.tsx
+++ app/build/TogglePublishPortfolio.tsx
@@ -1,20 +1,29 @@
-import { Switch, Tooltip } from "@mantine/core"
+import { Switch, Tooltip, Text } from "@mantine/core"
+import { useHover } from "@mantine/hooks"
 import { useState } from "react"
 
 const TogglePublishPortfilio = () => {
   const [checked, setChecked] = useState(false)
-
+  const { hovered: switchHovered, ref: wrapperRef } = useHover()
   return (
     <Tooltip
       label={checked ? "Hide your portfolio" : "Publish your portfilio"}
       withArrow
       position="bottom"
       color="violet"
+      opened={switchHovered}
     >
       <Switch
-        label="Publish"
+        wrapperProps={{ ref: wrapperRef }}
         radius="xl"
         color="violet"
+        label={
+          <Text
+            sx={(theme) => ({ color: theme.colorScheme === "dark" ? theme.white : theme.black })}
+          >
+            Publish
+          </Text>
+        }
         checked={checked}
         onChange={(event) => setChecked(event.currentTarget.checked)}
       />
