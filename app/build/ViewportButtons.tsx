0ad4c369fe80c15e846e5ec000e2d46f094bbf05
sp <sp@outsi.de>
2022-08-16 19:18:14 +0300

    Fix typos

--- app/build/ViewportButtons.tsx
+++ app/build/ViewportButtons.tsx
@@ -4,12 +4,12 @@ import { FaDesktop, FaMobileAlt } from "react-icons/fa"
 const ViewportButtons = (props: ActionIconProps) => {
   return (
     <Group spacing="xs">
-      <Tooltip label="Mobile view" color="violet" position="bottom" withArrow>
+      <Tooltip label="Mobile View" color="violet" position="bottom" withArrow>
         <ActionIcon {...props}>
           <FaMobileAlt />
         </ActionIcon>
       </Tooltip>
-      <Tooltip label="DesKtop view" color="violet" position="bottom" withArrow>
+      <Tooltip label="Desktop View" color="violet" position="bottom" withArrow>
         <ActionIcon {...props}>
           <FaDesktop />
         </ActionIcon>
